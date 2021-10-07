118dac353207ed9318af7a79745e06cde2646fae
Lyndsey Womack <lyndseywomack@lyndseys-air.lan>
2021-10-07 17:33:51 -0500

    updated

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -12,14 +12,14 @@ public class HelloController {
 @RequestMapping(value= "hello", method= {RequestMethod.GET, RequestMethod.POST})
 @ResponseBody
 public String hello(@RequestParam String name){
-    return "Hello " + name + "!";
+    return "Hello, " + name + "!";
 }
 
 // responds to /hello/LaunchCode
 @GetMapping("hello/{name}")
 @ResponseBody
 public String helloAgain(@PathVariable String name){
-    return "Hello " + name + "!";
+    return "Hello, " + name + "!";
 }
 
 @GetMapping("form")
